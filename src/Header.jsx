8bb3930fd79c4d7c0992d079fd5c5cc328c87cfc
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-11-14 02:40:22 +0600

    hero and footer section added

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import LWSLogo from "./assets/lws-logo-en.svg";
 export default function Header() {
   return (
     <nav className="py-6 md:py-8 fixed top-0 w-full bg-[#191D26] z-50">
-      <div className="container mx-auto flex items-center justify-between gap-x-6">
+      <div className="container lg:px-20 mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
           <img className="h-[45px]" src={LWSLogo} alt="Lws" />
         </a>
