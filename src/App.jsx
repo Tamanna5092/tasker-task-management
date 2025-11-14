be2cfd36dd0b3b9d12f29b31fca22c27d677d494
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-11-14 12:58:38 +0600

    taskboard setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBaoard from "./Task/TaskBaoard";
 
 function App() {
   return (
     <>
       <Header></Header>
       <HeroSection></HeroSection>
+      <TaskBaoard></TaskBaoard>
       <Footer></Footer>
     </>
   );
