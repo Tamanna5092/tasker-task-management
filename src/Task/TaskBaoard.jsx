1fe65bd313a743649f344a4e03b814d5b4411474
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-11-21 19:45:01 +0600

    add task

--- src/Task/TaskBaoard.jsx
+++ src/Task/TaskBaoard.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 export default function TaskBaoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description: "I want to learn react and next.js",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    console.log("add task", newTask);
+    setTasks([...tasks, newTask])
+    setShowAddModal(false)
+  }
+
   return (
     <section className="mb-20 container lg:px-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask}></AddTaskModal>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask></SearchTask>
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions></TaskActions>
-          <TaskList></TaskList>
+          <TaskActions
+            handleAddTask={() => setShowAddModal(true)}
+          ></TaskActions>
+          <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
     </section>
--- src/Task/TaskList.jsx
+++ src/Task/TaskList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
-export default function TaskList() {
+export default function TaskList({ tasks }) {
   return (
     <div className="overflow-auto">
       <table className="table-fixed overflow-auto xl:w-full">
@@ -30,34 +31,44 @@ export default function TaskList() {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-            <td>
-              <button>
-                {/* <FaStar color="yellow" />
-                <FaStar color="gray" /> */}
-              </button>
-            </td>
-            <td>title</td>
-            <td>
-              <div>description</div>
-            </td>
-            <td>
-              <ul className="flex justify-center gap-1.5 flex-wrap">
-                <li>
-                  <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
-                    tag
-                  </span>
-                </li>
-              </ul>
-            </td>
-            <td className="text-center">priority</td>
-            <td>
-              <div className="flex items-center justify-center space-x-3">
-                <button className="text-red-500">Delete</button>
-                <button className="text-blue-500">Edit</button>
-              </div>
-            </td>
-          </tr>
+          {tasks.map((task) => (
+            <tr
+              key={task.id}
+              className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
+            >
+              <td>
+                <button>
+                  {task.isFavorite ? (
+                    <FaStar color="yellow" />
+                  ) : (
+                    <FaStar color="gray" />
+                  )}
+                </button>
+              </td>
+              <td>{task.title}</td>
+              <td>
+                <div>{task.description}</div>
+              </td>
+              <td>
+                <ul className="flex justify-center gap-1.5 flex-wrap">
+                  {task.tags.map((tag) => (
+                    <li key={tag}>
+                      <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
+                        {tag}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </td>
+              <td className="text-center">{task.priority}</td>
+              <td>
+                <div className="flex items-center justify-center space-x-3">
+                  <button className="text-red-500">Delete</button>
+                  <button className="text-blue-500">Edit</button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
